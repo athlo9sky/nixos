f1f95d31f3b69a1dd31217ca1aa80de3e4a8171a
athlo9sky <athlo9sky@gmail.com>
2023-10-21 07:43:40 +0300

    settings up pulseaudio, pipewire

--- system/hardware/audio.nix
+++ system/hardware/audio.nix
@@ -1,6 +1,20 @@
 { config, pkgs, ... }:
 
 {
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  #sound.enable = true;
+  security.rtkit.enable = true;
+
+  hardware = {
+    pulseaudio = {
+      enable = false;
+      extraModules = [ pkgs.pulseaudio-modules-bt ];
+    };
+  };
+  
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 }
