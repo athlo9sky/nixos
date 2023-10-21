0f821b34fa5c3508d0ad5e96f6ed63dc675cdcee
athlo9sky <athlo9sky@gmail.com>
2023-10-21 09:30:43 +0300

    user packages

--- applications/user-packages.nix
+++ applications/user-packages.nix
@@ -4,6 +4,10 @@
   home-manager.users.athlo9sky = {
     home = {
       packages = with pkgs; [
+        telegram-desktop
+        discord-canary
+        discord-rpc
+        keepassxc
         tree
       ];
     };
